15246ce392394e17cf572e4ee72daf702318b5aa
KirillYabl <kirill-yablunovskii@mail.ru>
2020-05-06 22:15:13 +0300

    Common functions refactored

--- common_functions.py
+++ common_functions.py
@@ -6,18 +6,42 @@ def is_correct_answer(user_answer, correct_answer, limit):
     :param limit: number between 0 and 1, this is percentage of correct answers
     :return: bool, correct or no
     """
-    # 1. Split by '.'. 2. Take first. 3. Split by '('. 4. Take first. 5. Make lower
-    # '.' - If answer have more than one sentence, only one sentence will check
-    # '(' - This symbol usually means explanations. We won't check explanations
-    correct_answer = correct_answer.split('.')[0].split('(')[0].lower()
-    user_answer = user_answer.split('.')[0].split('(')[0].lower()
-    correct = 0
-    words = user_answer.split(' ')
-    for word in words:
-        if word in correct_answer:
-            correct += 1
+    def prepare_answer(answer):
+        """Do manipulations with answer for checking.
 
-    # I think half correct words is OK
-    if (correct / len(words)) >= limit:
+        1. Split by '.'. If answer have more than one sentence, only one sentence will check
+        2. Take first.
+        3. Split by '('. This symbol usually means explanations. We won't check explanations
+        4. Take first.
+        5. Make lower
+
+        :param answer: str, text of answer
+        :return: str, modified answer
+        """
+        first_sentence_of_answer = answer.split('.')[0]
+        answer_without_explanation = first_sentence_of_answer.split('(')[0]
+
+        return answer_without_explanation.lower()
+
+    # prepare answers
+    prepared_correct_answer = prepare_answer(correct_answer)
+    prepared_user_answer = prepare_answer(user_answer)
+
+    # sets of words
+    words_of_correct_answer = set(prepared_correct_answer.split(' '))
+    words_of_user_answer = set(prepared_user_answer.split(' '))
+
+    # correct words - intersection of sets
+    correct_words = words_of_correct_answer.intersection(words_of_user_answer)
+
+    # calculate len of correct and len of user answer
+    number_of_correct_words = len(correct_words)
+    number_of_user_answer_words = len(words_of_user_answer)
+
+    # calculate percentage of correct and compare with limit
+    percentage_of_correct_words = number_of_correct_words / number_of_user_answer_words
+    correct_words_more_or_equal_than_limit = percentage_of_correct_words >= limit
+
+    if correct_words_more_or_equal_than_limit:
         return True
     return False
