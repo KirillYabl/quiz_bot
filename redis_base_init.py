99f80233f0ea2ed14644d3fad47d4596a0b0b139
KirillYabl <kirill-yablunovskii@mail.ru>
2020-05-16 18:00:18 +0300

    Data with questions and answers was restructured again

--- redis_base_init.py
+++ redis_base_init.py
@@ -25,18 +25,22 @@ if __name__ == '__main__':
 
     with open(db_path, encoding='utf-8') as f:
         data = json.load(f)
-    data_keys = [int(num) for num in data.keys()]
-    logger.debug(f'data was read. Length: {len(data_keys)}')
+    logger.debug(f'data was read. Length: {len(data)}')
 
     redis_db = redis.Redis(host=redis_db_address, port=redis_db_port, password=redis_db_password)
 
-    for _ in range(db_record_count):
-        q_num = str(random.choice(data_keys))
-        question = str(data[q_num]['q']).encode('utf-8')
-        answer = str(data[q_num]['a']).encode('utf-8')
+    for questions_answer_num, question_data in enumerate(data.items()):
+        # all data can be very bigger for DB and maybe you don't want upload all data
+        if questions_answer_num == db_record_count:
+            break
+
+        question_num, question_answer = question_data
+        question = str(question_answer['q']).encode('utf-8')
+        answer = str(question_answer['a']).encode('utf-8')
 
         redis_db.hset(redis_hash_of_questions_and_answers_name, question, answer)
         redis_db.sadd(redis_set_of_questions_name, question)
+        logger.debug(f'Question {question_num} was record in DB')
 
     logger.debug(f'db size {redis_db.dbsize()}')
     redis_db.close()
